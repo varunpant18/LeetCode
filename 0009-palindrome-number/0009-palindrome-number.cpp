878723d2f9b0de7c7b925e1518e445053e91b067
Varun Pant <varunpant18@gmail.com>
2025-07-08 22:42:14 +0530

    Time: 5 ms (22.6%), Space: 8.7 MB (41.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,11 +3,10 @@ public:
     bool isPalindrome(int x) {
         int original = x;
         int rev_num = 0;
-        while(x > 0)
-        {
+        while (x > 0) {
             int d = x % 10;
-            if (rev_num > (INT_MAX - d) / 10) 
-            { return false;
+            if (rev_num > (INT_MAX - d) / 10) {
+                return false;
             }
             rev_num = rev_num * 10 + d;
             x /= 10;
