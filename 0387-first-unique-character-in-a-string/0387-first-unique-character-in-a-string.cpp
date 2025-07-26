e6d4de26f27a9e34ab697fd3cb3e34a04b54235d
Varun Pant <varunpant18@gmail.com>
2025-07-26 23:07:06 +0530

    Time: 9 ms (61.14%), Space: 14.6 MB (89.77%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,15 +3,16 @@ public:
     int firstUniqChar(string s) {
         for (int i = 0; s[i] != '\0'; i++) {
             bool isUnique = true;
+
             for (int j = 0; s[j] != '\0'; j++) {
                 if (i != j && s[i] == s[j]) {
                     isUnique = false;
                     break;
                 }
             }
-            if (isUnique) {
-                return i; 
-            }
+
+            if (isUnique)
+                return i;
         }
         return -1;
     }
