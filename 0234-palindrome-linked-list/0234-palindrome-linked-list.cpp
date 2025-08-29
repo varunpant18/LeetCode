c4d0a01d787c42c476d51074427519da44e68af4
Varun Pant <varunpant18@gmail.com>
2025-08-30 00:48:06 +0530

    Time: 13 ms (10.89%), Space: 127.4 MB (25.25%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> s;
+        ListNode* p = head;
+
+        while(p!=NULL)
+        {
+            s.push(p->val);
+            p=p->next;
+        }
+        while(head && !s.empty()){
+            if(s.top() != head->val) return false;
+            else {
+                s.pop();
+                head = head->next;
+            }   
+        }
+        return true;
+    }
+};
