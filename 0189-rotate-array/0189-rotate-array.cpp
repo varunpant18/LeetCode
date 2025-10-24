3eab2c881660dacdc9fae3d3c47318db3b4b11ba
Varun Pant <varunpant18@gmail.com>
2025-10-24 23:03:28 +0530

    Time: 0 ms (100%), Space: 29.5 MB (60.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,14 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> temp(nums.size());
-
-        for (int i = 0; i < nums.size(); i++) {
-            temp[(i + k) % nums.size()] = nums[i];
-        }
-
-        nums = temp;
+        int n = nums.size();
+        k %= n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
